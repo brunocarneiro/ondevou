73ae85a4ac1e692ba622b65eb4aabe3a93a57a21
ondevou <ondevou@f02dfd39-7f98-4e71-868b-6b3d37418394>
2011-08-03 01:19:36 +0000

    Adicionado acao nas abas de lugar.
    git-svn-id: http://ondevou.unfuddle.com/svn/ondevou_repo1/trunk@23 f02dfd39-7f98-4e71-868b-6b3d37418394

--- site_parent/site/src/main/java/com/griggy/rest/BaseConversor.java
+++ site_parent/site/src/main/java/com/griggy/rest/BaseConversor.java
@@ -15,6 +15,11 @@ import com.powerlogic.jcompany.controller.rest.conversors.simple.PlcJsonConverso
 import com.powerlogic.jcompany.domain.validation.PlcMessage;
 import com.powerlogic.jcompany.domain.validation.PlcMessage.Cor;
 
+/**
+ *Conversor base para todos os servicos
+ *@author Bruno Carneiro
+ */
+
 @SPlcConversor
 @QPlcConversorMediaType( { "application/json", "*/*" })
 public class BaseConversor extends PlcJsonConversor<BaseController<Object, Object>> {
@@ -30,6 +35,21 @@ public class BaseConversor extends PlcJsonConversor<BaseController<Object, Objec
 
 		response.put("template", template.get(getUcName()));
 		response.put("data", container.getEntity());
+		response.put("messages", getMessages());
+		try {
+			writeResponse(outputStream, response);
+		} catch (IOException e) {
+			writeException(container, outputStream, new PlcMessage(e.getMessage(), Cor.msgVermelhoPlc));
+		}
+	}
+	
+	@Override
+	public void writeEntityCollection(BaseController<Object, Object> container, OutputStream outputStream) {
+		
+		Map<String, Object> response = new LinkedHashMap<String, Object>();
+
+		response.put("template", template.get(getUcName()));
+		response.put("data", container.getEntityCollection());
 		response.put("messages", getMessages());
 		try {
 			writeResponse(outputStream, response);
---
+++ site_parent/site/src/main/webapp/usuarioman.js
@@ -0,0 +1,40 @@
+/**
+ * Ao iniciar a pagina
+ * @author Bruno Carneiro
+ */
+$(document).ready(function(){
+
+	aoClicarAba("visitado");
+	aoClicarAba("favorito");
+	aoClicarAba("avaliado");
+	aoClicarAba("desejado");
+	aoClicarAba("agenda");
+	
+});
+
+function aoClicarAba(nomeAba){
+	$("#"+nomeAba).click(function(){
+		
+		$.ajax({url:'http://localhost:8080/site/soa/service/api.lugarUsuario?usuario='+getParam('id')+'&'+nomeAba+'=1',
+			dataType:'json',
+			success: function(result){
+					$('.libraryItems').html("");
+					parseado=Mustache.to_html(getTemplate(), result).replace(/^\s*/mg, '');
+					$('.libraryItems').html(parseado);
+					//trocando a selecao da aba
+					$('#libraryNavigation li.current').removeClass('current');
+					$("#"+nomeAba).parent().addClass('current');
+				}
+			}
+		);
+	});
+}
+
+function getTemplate(){
+	return '<ul class=" libraryItems artistsLarge">{{#data}}{{#lugar}}<li id="r6_1053512" class=" first"><a href="lugar?id="><span class="pictureFrame"><span class="image">'
+	+'<img width="126" height="126" src="" alt="">'
+	+'</span><span class="overlay"></span></span><strong class="name">{{nome}}</strong></a> <a rel="nofollow" class="plays" href=""><span dir="ltr">(609&nbsp;opiniões)</span></a>'
+	+'<a href="" class="playbutton ">'
+	+'<img width="13" height="13" src="" alt="Tocar" class="transparent_png play_icon">'
+	+'</a><a class="remove" title="" href=""><img width="13" height="13" src="" class="transparent_png dismiss_dark_icon"></a></li>{{/lugar}}{{/data}}</ul>';
+}
