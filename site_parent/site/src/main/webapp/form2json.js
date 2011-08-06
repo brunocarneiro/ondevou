2606e053cb25d767a8b022c1ab57642c65a9ea75
ondevou <ondevou@f02dfd39-7f98-4e71-868b-6b3d37418394>
2011-08-06 18:08:56 +0000

    #bruno# organizacao de estrutura de js e página de opiniao.
    git-svn-id: http://ondevou.unfuddle.com/svn/ondevou_repo1/trunk@30 f02dfd39-7f98-4e71-868b-6b3d37418394

---
+++ site_parent/site/src/main/webapp/form2json.js
@@ -0,0 +1,26 @@
+/**
+ * @author Bruno Carneiro
+ * 
+ */
+
+
+/**
+ * Transforma um form em um json.
+ */
+function form2json(form){
+	var array=form.serializeArray();
+	var parent={},json={};
+	for(var i=0; i<array.length; i++){
+		if(array[i]["name"].indexOf('_')>0){
+			var split=array[i]["name"].split("_");
+			var innerObject={};
+			innerObject[split[1]]=array[i]["value"]
+			json[split[0]]=innerObject;
+		}
+		else{
+			json[array[i]["name"]]=array[i]["value"];
+		}
+	}
+	parent[form.attr("id")]=json;
+	return JSON.stringify(parent);
+}
--- site_parent/site/src/main/webapp/usuario.js
+++ site_parent/site/src/main/webapp/usuario.js
@@ -1,16 +1,32 @@
 $(function(){
 	$("#opinion").click(function(){$(getOpinionTemplate()).dialog({modal:true, height:500, width:600, create:
 		function(event,ui){
-			$(" #notaAmbiente input").rating();
-			$(" #notaAtendimento input").rating();
-			$(" #notaComida input").rating();
-			$(" #notaBebida input").rating();
-			$(" #notaCustoBeneficio input").rating();
+			$("input[type=radio]").rating();
 		}
 	})});
 	$("#opinion").button();
 	
-	
+	$("#enviarOpiniao").live("click",function(){
+		$.ajax(
+				{
+					 url:'http://localhost:8080/site/soa/service/api.lugarUsuario',
+					 data:form2json($("#lugarUsuario")),
+					 type:'POST',
+					 dataType:'json',
+					 success: function(data){
+						 if(data.messages["erro"])
+							 $("#msg").append(data.messages["erro"]);
+						 if(data.messages["sucesso"]){
+							 alert(data.messages["sucesso"])
+							 $("#msg").append(data.messages["sucesso"]);
+							 if(data.messages["sucesso"].length>0){
+								 //fechando a janela
+								 $(getOpinionTemplate()).dialog().close();
+							 }
+						 }
+					 }
+				});
+	});
 });
 
 
