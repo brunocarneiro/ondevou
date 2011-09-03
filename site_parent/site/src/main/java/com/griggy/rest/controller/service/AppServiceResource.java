ef46b1ae182ff7a608fe226fa4fd2e25f1499be8
ondevou <ondevou@f02dfd39-7f98-4e71-868b-6b3d37418394>
2011-09-03 15:34:27 +0000

    #BRUNO# ServiceResource para adquirir o id do usuario pelo usuario da sessão. Funcionou!
    git-svn-id: http://ondevou.unfuddle.com/svn/ondevou_repo1/trunk@58 f02dfd39-7f98-4e71-868b-6b3d37418394

---
+++ site_parent/site/src/main/java/com/griggy/rest/controller/service/AppServiceResource.java
@@ -0,0 +1,41 @@
+package com.griggy.rest.controller.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+import org.jboss.resteasy.annotations.cache.NoCache;
+
+import com.plc.site.commons.AppUserProfileVO;
+import com.powerlogic.jcompany.commons.config.qualifiers.QPlcDefaultLiteral;
+import com.powerlogic.jcompany.commons.util.cdi.PlcCDIUtil;
+import com.powerlogic.jcompany.controller.rest.api.controller.IPlcController;
+import com.powerlogic.jcompany.controller.rest.api.conversor.IPlcConversor;
+import com.powerlogic.jcompany.controller.rest.service.PlcServiceResource;
+import com.powerlogic.jcompany.controller.rest.service.PlcServiceConstants.PATH_PARAM;
+
+@Path("/rest")
+public class AppServiceResource extends PlcServiceResource {
+	
+	@GET
+	@Path("/{" + PATH_PARAM.CONTROLLER_NAME + ":" + NAME_PATTERN + "}/self")
+	@NoCache
+	@SuppressWarnings("unchecked")
+	@Override
+	public <E, I> Response retrieveResource() {
+		
+		IPlcController<E, I> controller = getController();
+
+		IPlcConversor<IPlcController<E, I>> conversor = getConversor();
+
+		conversor.readEntity(controller, null);
+
+		AppUserProfileVO appUserProfile = PlcCDIUtil.getInstance().getInstanceByType(AppUserProfileVO.class, QPlcDefaultLiteral.INSTANCE);
+
+		controller.retrieve((I) appUserProfile.getUsuario().getId());
+
+		return getResponse(controller);
+	}
+	
+
+}
